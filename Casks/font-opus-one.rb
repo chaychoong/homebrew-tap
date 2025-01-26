acdd89ede34b53aee5429cedc1f59b14839269d5
Chay Choong <chaychoong@gmail.com>
2025-01-26 17:48:32 +0800

    bump: font-opus-one 1.3
    Also change ttf -> otf

--- Casks/font-opus-one.rb
+++ Casks/font-opus-one.rb
@@ -1,15 +1,15 @@
 cask "font-opus-one" do
-  version "1.2"
-  sha256 "c767ded11b24f63ffb548ea8fbba9620c450e12165cb65035e907c68821380ae"
+  version "1.3"
+  sha256 "ed64418fb4e46392195dbae716711b48535c6246e154c0b62bc8d9cfccdc3446"
 
   url "https://github.com/chaychoong/OpusOne/archive/refs/tags/v#{version}.tar.gz"
   name "Opus One"
   homepage "https://github.com/chaychoong/OpusOne"
 
-  font "OpusOne-#{version}/fonts/ttf/OpusOne-Regular.ttf"
-  font "OpusOne-#{version}/fonts/ttf/OpusOne-RegularItalic.ttf"
-  font "OpusOne-#{version}/fonts/ttf/OpusOne-Bold.ttf"
-  font "OpusOne-#{version}/fonts/ttf/OpusOne-BoldItalic.ttf"
+  font "OpusOne-#{version}/fonts/otf/OpusOne-Regular.otf"
+  font "OpusOne-#{version}/fonts/otf/OpusOne-RegularItalic.otf"
+  font "OpusOne-#{version}/fonts/otf/OpusOne-Bold.otf"
+  font "OpusOne-#{version}/fonts/otf/OpusOne-BoldItalic.otf"
   font "OpusOne-#{version}/fonts/variable/OpusOneVF[wght,ital].ttf"
 
   # No zap stanza required
